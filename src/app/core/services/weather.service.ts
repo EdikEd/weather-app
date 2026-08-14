f423ee7dfbb17970716f16773717c0791e804ae6
Eduard Erhart <edik.ergart2017@gmail.com>
2026-08-14 16:15:41 +0200

    api endpoint test

--- src/app/core/services/weather.service.ts
+++ src/app/core/services/weather.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.local';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = 'https://data.api.xweather.com';
+  private readonly clientId = environment.xweather.clientId;
+  private readonly clientSecret = environment.xweather.clientSecret;
+
+
+  getCurrentWeather(city: string) {
+    const params = new HttpParams()
+      .set('client_id', this.clientId)
+      .set('client_secret', this.clientSecret)
+      .set('units', 'metric')
+      return this.http.get(
+        `${this.baseUrl}/observations/${encodeURIComponent(city)}`,
+        { params }
+      )
+  }
 
   constructor() { }
 }
