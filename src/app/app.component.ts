ef243794b9d40cd7086c6658a718621b86b2083d
Eduard Erhart <edik.ergart2017@gmail.com>
2026-08-14 17:45:07 +0200

    added preset of cities to select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,57 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WeatherService } from './core/services/weather.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Weather App';
-  private weatherService = inject(WeatherService);
+  private weatherService = inject(WeatherService);  
+  weather: any;
+  city = 'new york, ny';
+  error = '';
+  loading = false;
+
+  cities = [
+    { label: 'New York', value: 'new+york,ny' },
+    { label: 'London', value: 'london,uk' },
+    { label: 'Bratislava', value: 'bratislava,sk' },
+    { label: 'Berlin', value: 'berlin,de' },
+  ];
+
+  selectCity(city: string) {
+    this.city = city;
+    this.loadWeather();
+  }
 
   constructor() {
-    this.weatherService.getCurrentWeather('bratislava,sk').subscribe(
-      res => console.log(res)
-    )
+
+  }
+
+  loadWeather() {
+    this.loading = true;
+    this.error = '';
+
+    this.weatherService.getCurrentWeather(this.city)
+      .subscribe({
+        next: (result: any) => {
+          this.weather = result.response;
+          this.loading = false;
+        }
+      })
+      error: () => {
+        this.error = 'Could not load weather for this location';
+        this.loading = false;
+      }
+  }
+
+  ngOnInit() {
+    this.loadWeather();
   }
 
 
